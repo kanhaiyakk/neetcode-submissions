0eeacd0c487570af43eb0e9f3aaab4d0f49aa1a4
Kanhaiya Kumar <88824128+kanhaiyakk@users.noreply.github.com>
2026-09-21 06:45:40 +0530

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+      Map<Integer, Integer> map=new HashMap<>();
+      for(int num: nums){
+        map.put(num, map.getOrDefault(num, 0)+1);
+      } 
+      for(int num: nums){
+      if(map.containsKey(num) && map.get(num)>1){
+        return true;
+      }
+      }
+      return false;
+    }
+}
